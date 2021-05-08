438ec5866d372c72f536893d977c6355f6fa5896
Piyush Onkar <piyushonkar1998@gmail.com>
2021-05-08 07:25:27 +0530

    Run code and note down the results

--- Assignment3/cuda.cu
+++ Assignment3/cuda.cu
@@ -1,8 +1,11 @@
 #include <bits/stdc++.h>
-#define N 10
+#include <cuda_runtime.h>
+#include <cuda_runtime_api.h>
+#include <cuda.h>
 using std::cout;
+using namespace std;
 
-__global__ void matrixMul(float *a, float *b, float *c) {
+__global__ void matrixMul(float *a, float *b, float *c, int N) {
   printf("%s","Hello World");
   int row = blockIdx.y * blockDim.y + threadIdx.y;
   int col = blockIdx.x * blockDim.x + threadIdx.x;
@@ -15,13 +18,13 @@ __global__ void matrixMul(float *a, float *b, float *c) {
   }
 }
 
-void init_matrix(float *m){
+void init_matrix(float *m, int N){
   for (int i=0;i<N*N;i++){
     m[i] = ((float) rand()) / (float) RAND_MAX ;
   }
 }
 
-void print_matrix(float * a){
+void print_matrix(float * a, int N){
   for(int i=0;i<N;i++){
     for(int j=0;j<N;j++){
       cout << a[i*N+j]<<"\t";
@@ -30,8 +33,24 @@ void print_matrix(float * a){
   }
 }
 
-int main() {
+void verify_result(float *a, float *b, float *c, int N){
+    float tmp;
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            tmp = 0;
+            for(int k = 0; k < N; k++){
+                tmp += a[i * N + k] * b[k * N + j];
+            }
+            assert(tmp == c[i * N + j]);
+        }
+    }
+}
 
+int main(int argc, char* argv[]) {
+
+  int N = atoi(argv[1]);
+  std::cout << std::fixed;
+  std::cout << std::setprecision(5);
   // host memory
   float *host_A, *host_B , *host_C;
   host_A = new float[N*N];
@@ -39,8 +58,8 @@ int main() {
   host_C = new float[N*N];
 
   // Initialize matrices
-  init_matrix(host_A);
-  init_matrix(host_B);
+  init_matrix(host_A,N);
+  init_matrix(host_B,N);
 
   // Allocate device memory
   float *device_A, *device_B , *device_C;
@@ -63,22 +82,35 @@ int main() {
   dim3 blocks(BLOCKS, BLOCKS);
 
   // Launch kernel
-  matrixMul<<<blocks, threads>>>(device_A, device_B, device_C);
+  auto s = chrono::steady_clock::now();
+  matrixMul<<<blocks, threads>>>(device_A, device_B, device_C,N);
+  auto e = chrono::steady_clock::now();
+  auto diff = e - s;
+  double mSecs =chrono::duration <double, milli> (diff).count();
 
   // Copy back to the host
   cudaMemcpy(host_C, device_C, N*N*sizeof(float), cudaMemcpyDeviceToHost);
 
   cout << "COMPLETED SUCCESSFULLY\n";
-
-
+  verify_result(host_A, host_B, host_C, N);
 
   // Free memory on device
   cudaFree(device_A);
   cudaFree(device_B);
   cudaFree(device_C);
 
-  // print_matrix(host_A);
-  // print_matrix(host_B);
-  // print_matrix(host_C);
+  cout <<"Size = "<<N;
+  cout<<"\n \n";
+  cout<<"A = \n";
+  print_matrix(host_A,N);
+  cout<<"\n";
+  cout<<"B = \n";
+  print_matrix(host_B,N);
+  cout<<"\n";
+  cout<<"C = \n";
+  print_matrix(host_C,N);
+  cout<<"\n";
+  cout<<"Time in ms = "<<mSecs;
+  cout<<"\n";
   return 0;
 }
